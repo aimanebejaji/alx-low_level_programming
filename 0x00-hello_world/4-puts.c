8a161fbc93be86b9612ca73983cf074ac8b35d36
aimanebajaji <aimane.bejaji.peronal@gmail.com>
2023-10-09 21:36:24 +0000

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a lline using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Progaming is like building a multilingual puzzle");
+	return (0);
+}
