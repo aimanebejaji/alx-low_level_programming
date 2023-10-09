60e143d75036a5806059a640750faad2d5605999
aimanebajaji <aimane.bejaji.peronal@gmail.com>
2023-10-09 21:49:20 +0000

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - A programm that prints a line using printf
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }	
